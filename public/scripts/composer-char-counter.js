e18262d54bfcf4386a811d8405a56b59a9731607
Dkell88 <DarrenKelly1188@gmail.com>
2022-04-26 18:47:35 -0700

    Ensure the tweet article will handle 140 chars without pushing past the article border

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,19 +2,14 @@
 $(() => {
   console.log("DOM ready");
 
-  $('#tweet-text').keyup(() =>{
-    let count = 140 - $('#tweet-text').val().length;
-    $('.counter').val(count);
-    console.log($('.counter').val());
-    if (count < 0) {
-      console.log("Adding red class");
-      $('.counter').addClass('red-text');
-    }
-    if (count > -1 && $('.counter').hasClass('red-text')) {
-      console.log("Removing red class");
-      $('.counter').removeClass('red-text');
-    }
+  $('#tweet-text').keyup(function() {
 
+    let count = 140 - $(this).val().length;
+    let counter = $(this).siblings().children(".counter");
+    counter.val(count);
+
+    if (count < 0) counter.addClass('red-text');
+    if (count > -1 && counter.hasClass('red-text')) counter.removeClass('red-text');
+  
   });
-
 });
